a4e9672a6227de3792bb606742798ec20a04c9fd
moteloumka <73367393+moteloumka@users.noreply.github.com>
2021-02-23 15:26:53 +0100

    gjh

--- src/ch/epfl/tchu/game/Trip.java
+++ src/ch/epfl/tchu/game/Trip.java
@@ -1,5 +1,16 @@
 package ch.epfl.tchu.game;
 
-public class Trip {
+import ch.epfl.tchu.Preconditions;
 
-}
+public final class Trip {
+    private Station from;
+    private Station to;
+    private int points;
+    public Trip(Station from, Station to, int points){
+        if(from==null||to==null){
+            throw new NullPointerException();
+            }
+        Preconditions.checkArgument(points>0);
+        }
+    }
+
