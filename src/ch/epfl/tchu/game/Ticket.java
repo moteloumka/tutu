6d48622895e53ed8c0e3c5a6b3e5e811629dba6a
Guillen-EPFL <57261742+Guillen-EPFL@users.noreply.github.com>
2021-02-25 13:03:46 +0100

    did the documentation for the ticket class. also sniff my ass

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -1,17 +1,17 @@
 package ch.epfl.tchu.game;
 import java.util.*;
 
-
 public final class Ticket implements Comparable<Ticket> {
     private List<Trip> trips;
     private String text;
 
+    /**
+     * @param trips list of trips the ticket contains. Must be at least one.
+     */
     public Ticket(List<Trip> trips){
-
        if (trips==null || trips.size()==0){
             throw new IllegalArgumentException();
         }
-
            //checking that all trips leave from a station with the same name
            this.trips = Objects.requireNonNull(trips, "trips must not be null");
            for (Trip t : trips) {
@@ -23,17 +23,27 @@ public final class Ticket implements Comparable<Ticket> {
        }
     }
 
+    /**
+     * constructor for ticket containing only one trip
+     * @param from the departure station of the ticket (must not be null)
+     * @param to the arrival station of the ticket (must not be null)
+     * @param points the nbr of points that can be gained by connecting the two stations (must not be null)
+     */
     public Ticket(Station from, Station to, int points){
             this(List.of(new Trip (from, to, points)));
     }
 
-
+    /**
+     * @return the text of the trip (departure station to arrival stations and points)
+     */
     public String text() {
         return this.text;
     }
 
-
-
+    /**
+     * does the computation for the text method
+     * @return the text of ticket
+     */
     private String computeText(){
         String ticketText = "";
         ticketText += this.trips.get(0).from().toString() + " - ";
@@ -50,16 +60,21 @@ public final class Ticket implements Comparable<Ticket> {
             ticketText+= this.trips.get(0).to().toString();
             ticketText += " (" + this.trips.get(0).points() + ")";
         }
-
         return ticketText;
     }
 
-
+    /**
+     * @return the same as the text() method
+     */
     public String toString(){
         return this.text;
     }
 
-
+    /**
+     * @param connectivity tells us if the stations of the trip have been connected
+     * @return the nbr of points from the trip if it only contains one, the nbr with the most points from all the trips if
+     * more than one trip in the ticket
+     */
     public int points (StationConnectivity connectivity){
         List<Integer> pointsList = new ArrayList<>();
         for (Trip t : trips){
@@ -68,6 +83,11 @@ public final class Ticket implements Comparable<Ticket> {
         return Collections.max(pointsList);
     }
 
+    /**
+     * compares both tickets by alphabetical order
+     * @param that the ticket with which we're comparing this ticket
+     * @return negative int if this comes before that, 0 if same ticket, positive int if this comes after that
+     */
     @Override
     public int compareTo(Ticket that) {
         return this.text.compareTo(that.text);
