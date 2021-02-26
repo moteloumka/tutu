084c195bd165b3a1a811d23a0935b1e6c602b62a
moteloumka <73367393+moteloumka@users.noreply.github.com>
2021-02-26 13:50:07 +0100

    -comments added poggy

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -17,9 +17,9 @@ public final class Ticket implements Comparable<Ticket> {
      */
     public Ticket(List<Trip> trips){
         Preconditions.checkArgument(!(trips==null || trips.size()==0));
-
         //checking that all trips leave from a station with the same name
         this.trips = Objects.requireNonNull(trips, "trips must not be null");
+
         for (Trip t : trips) {
             if (!(trips.get(0).from().name().equals(t.from().name()))) {
                 throw new IllegalArgumentException();
