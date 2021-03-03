90b808fe28f35bb29738aee629b4f371669b6c6d
moteloumka <73367393+moteloumka@users.noreply.github.com>
2021-03-03 21:27:55 +0100

    work be going slowly

--- src/ch/epfl/tchu/game/RouteTest.java
+++ src/ch/epfl/tchu/game/RouteTest.java
@@ -12,10 +12,23 @@ class RouteTest {
 
     @Test
     void id() {
+        Route r1 = ChMap.routes().get(0);
+        Route r2 = ChMap.routes().get(10);
+        assertEquals("AT1_STG_1",r1.id());
+        assertEquals("BEL_LUG_2",r2.id());
     }
 
     @Test
     void station1() {
+        Station station1 =  new Station(0,"TEST1");
+        Station station2 =  new Station(0,"TEST2");
+        String id1 = station1.name();
+        String id2 = station2.name();
+
+        Route route = new Route(id1,station1,station2)
+        Route r1 = ChMap.routes().get(0);
+        Route r2 = ChMap.routes().get(10);
+
     }
 
     @Test
